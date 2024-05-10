7073122527b34a16109a06286ee913410ebd04f2
verss21 <arjunafrizal01@gmail.com>
2024-05-10 10:11:20 +0700

    step by step

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -26,5 +26,12 @@ public:
 	//push operation:  insert an element onto the top of the stack
 	int push(int value) {
 		Node* newNode = new Node(); // 1. allocate memory for the new node
+		newNode->data = value; //2. assign value
+		newNode->next = top; // set the next pointer of the new node to the current top node
+		top = newNode; // update the top pointer to the new node
+		cout << "Push Value: " << value << endl;
+		return value;
 	}
+
+	
 };
