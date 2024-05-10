9825bc8d9ea846118c0fdce4b2b3cfdd5270b42f
verss21 <arjunafrizal01@gmail.com>
2024-05-10 10:14:18 +0700

    menghapus element dari stack

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -32,6 +32,13 @@ public:
 		cout << "Push Value: " << value << endl;
 		return value;
 	}
-
 	
+	// pop operation: remove the topmost element from the stack
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next; // update the top pointer to the next node
+	}
 };
