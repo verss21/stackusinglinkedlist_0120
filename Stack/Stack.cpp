9767fe209070b524452d53e9d5b3ee10f2914c5e
verss21 <arjunafrizal01@gmail.com>
2024-05-10 10:25:36 +0700

    case 1

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -80,4 +80,10 @@ int main() {
 		cin >> choice;
 
 		switch (choice) {
-		
+		case 1:
+			cout << "Enter the value to push: ";
+			cin >> value;
+			stack.push(value); //push the entered value onto the stack
+		}
+	}
+}
