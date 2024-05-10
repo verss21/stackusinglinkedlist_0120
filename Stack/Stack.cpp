fe8c5ede6069b2085f05123aa884904c22ce92d4
verss21 <arjunafrizal01@gmail.com>
2024-05-10 10:03:03 +0700

    Class node public

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 
 using namespace std;
+
+//node class representing a single node in linked list
+class Node {
+public:
+	int data;
+	Node* next;
+
+	Node() {
+		next = NULL;
+	}
+};
