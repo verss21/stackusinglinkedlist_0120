07a6187084011939560decce94038035beaac6ba
verss21 <arjunafrizal01@gmail.com>
2024-05-10 10:16:18 +0700

    if

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -45,6 +45,6 @@ public:
 
 	// peek/top operation: Retrieve the value of the topmost element without removing it
 	void peek() {
-
-	}
-};
+		if (top == NULL) {
+			cout << "List is empty." << endl;
+		}
